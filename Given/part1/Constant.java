d441392187ce35e8b48c603077f11c7bd7ad80a4
Shwoobie <atdwashington@gmail.com>
2014-02-09 19:47:54 -0800

    changed that ->

--- Given/part1/Constant.java
+++ Given/part1/Constant.java
@@ -2,7 +2,7 @@ public class Constant extends Seq{
 	//protected int num;
 	//protected int value;
 	public void Constant( int num, int value ){
-		this.number = num;
+		this.num = num;
 		if (num == 0) {
 			this.value = 0;
 		}
