f423848bd33bb600e63e539db3c63e144fdecf23
Shwoobie <atdwashington@gmail.com>
2014-02-09 20:19:56 -0800

    made things private

--- Given/part1/Jumble.java
+++ Given/part1/Jumble.java
@@ -1,10 +1,10 @@
 public class Jumble extends Seq{
 	//protected int [] values;
-	public Jumble( int [] values){
+	private Jumble( int [] values){
 		//this.values = new int [](values.length);
 		System.arraycopy(values, 0, this.values, 0, values.length());
 	}
-	public String toString(){
+	private String toString(){
 		System.err.print("{ "+values.length+" :");
 			for (int i = 0; i < values.length; i++){
 				System.err.print(" "+values[i]);
