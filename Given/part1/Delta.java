fcc9d228f7aac7f3e24dbd7fedfe066b9ebebc28
Shwoobie <atdwashington@gmail.com>
2014-02-09 20:40:57 -0800

    public put back

--- Given/part1/Delta.java
+++ Given/part1/Delta.java
@@ -2,7 +2,7 @@ public class Delta extends Seq{
 	//protected int num;
 	//protected int initial;
 	//protected int delta;
-	private Delta( int num, int initial, int delta ){
+	public Delta( int num, int initial, int delta ){
 		this.num = num;
 		if (num == 0){
 			this.initial = 0;
@@ -12,7 +12,7 @@ public class Delta extends Seq{
 		this.initial = initial;
 		this.delta = delta;
 	}
-	private String toString(){
+	public String toString(){
 		System.err.print("< "+num+" : "+ initial+ " &"+ delta+" >");
 	}
 
