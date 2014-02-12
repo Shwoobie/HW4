6321468dee6e09e1a53d11c518f95abc270db31b
Shwoobie <atdwashington@gmail.com>
2014-02-11 20:52:11 -0800

    curr

--- Given/part3/JumbleUser.java
+++ Given/part3/JumbleUser.java
@@ -4,14 +4,16 @@ public class JumbleUser{
 		JumbleIt user = new JumbleIt(j);
 		int longest = 0;
 		int counter = 0;
+		int curr;
 		int prev = -999799;
 		while(user.hasNext()){
-			if(user.next() > prev){
+			if((curr = user.next()) > prev){
 				counter++;
 			}
 			else{
 				counter = 1;	
 			}
+			prev = curr;
 			if (counter > longest){
 				longest = counter;
 			}
