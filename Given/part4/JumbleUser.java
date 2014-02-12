474f26606f9fdd611283b42a45f515a63b1717db
Shwoobie <atdwashington@gmail.com>
2014-02-11 22:31:43 -0800

    Uncommitted changes

--- Given/part4/JumbleUser.java
+++ Given/part4/JumbleUser.java
@@ -26,23 +26,23 @@ public class JumbleUser{
 		return longest;
 	}
 
-	public static int lengthLongestNDCSS1(Jumble j)throws UsingIteratorPastEndException{
+	public static int lengthLongestNDCSS1(Jumble j){
 		JumbleIt user = new JumbleIt(j);
 		int longest = 0;
 		int counter = 0;
 		int curr;
 		int prev = -999799;
 		while(user.hasNext()){
-				if((curr = user.next()) >= prev){
-					counter++;
-				}
-				else{
-					counter = 1;	
-				}
-				prev = curr;
-				if (counter > longest){
-					longest = counter;
-				}
+			if((curr = user.next()) >= prev){
+				counter++;
+			}
+			else{
+				counter = 1;	
+			}
+			prev = curr;
+			if (counter > longest){
+				longest = counter;
+			}
 		}
 		return longest;
 	}
