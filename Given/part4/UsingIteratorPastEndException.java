edea969d7f85fb61e4a7691fececbfb302b3ea6b
Shwoobie <atdwashington@gmail.com>
2014-02-11 22:33:29 -0800

    we just pcopied and pasted

--- Given/part4/UsingIteratorPastEndException.java
+++ Given/part4/UsingIteratorPastEndException.java
@@ -2,7 +2,6 @@
 
 class UsingIteratorPastEndException extends Exception {
 	static final long serialVersionUID = 98L;
-	public UsingIteratorPastEndException(String msg){
-	super(msg); // call constructor in superclass (i.e., base class); it saves message and a bit more.
+	public UsingIteratorPastEndException(){
 	}
 }
